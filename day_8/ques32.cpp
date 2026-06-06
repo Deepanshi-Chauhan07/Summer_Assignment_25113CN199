f4f72e454201ff34b13847cf88f3ce12a41de6dc
Deepanshi-Chauhan07 <chauhandeepanshi0011@gmail.com>
2026-06-06 12:49:30 -0900

    day_8 assignment is uploaded

---
+++ day_8/ques32.cpp
@@ -0,0 +1,15 @@
+// to print repeated number pattern.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no. of rows:"<<endl;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
